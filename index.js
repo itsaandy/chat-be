0a708016684820ae49082b3ebeb6aac5446c4b16
adityapokharel <adityapokharel97@gmail.com>
2019-11-04 19:22:02 +1100

    Change port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 80;
 
 app.get('/', (req, res) => {
     res.send('<h1>backend active.</h1>');
