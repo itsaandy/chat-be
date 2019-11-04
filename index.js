5aa6ae2b0091bcd59b5121b2bc6d6e116caa4e50
adityapokharel <adityapokharel97@gmail.com>
2019-11-04 19:08:02 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+    res.send('<h1>backend active.</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log("A user connected!!");
+    socket
+    .on('disconnect', () => {
+        console.log('a user disconnected');
+    })
+    .on('sendChat', (chat) => {
+        socket.broadcast.emit('receiveChat', chat);
+    })
+})
+
+http.listen(port, () => {
+    console.log('Listening on port: ', port);
+});
